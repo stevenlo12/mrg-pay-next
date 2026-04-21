d3b6441e50c0f9cfefd4873d3d20ce0000495a8f
Teddy Piter <53746400+teddypiter@users.noreply.github.com>
2026-04-21 09:49:57 +0700

    Update default locale in routing configuration from English to Indonesian for improved internationalization support.

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,6 +2,6 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["en", "id"],
-  defaultLocale: "en",
+  defaultLocale: "id",
   localePrefix: "as-needed",
 });
